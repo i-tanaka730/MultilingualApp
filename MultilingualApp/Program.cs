28bfa5782de0a96b44651e7129ce87e8f73ccfae
i-tanaka730 <pttp730@gmail.com>
2021-12-27 23:14:11 +0900

    feat: リソースを使用して日本語・英語・フランス語のHelloWorldを出力するように対応。

--- MultilingualApp/Program.cs
+++ MultilingualApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using System.Threading;
 
 namespace MultilingualApp
 {
@@ -6,7 +8,18 @@ namespace MultilingualApp
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("CurrentCulture: {0}", Thread.CurrentThread.CurrentCulture.Name);
+
+			// 日本語
+			Console.WriteLine(Strings.HelloWorld);
+
+			// 英語
+			Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
+			Console.WriteLine(Strings.HelloWorld);
+
+			// フランス語
+			Thread.CurrentThread.CurrentUICulture = new CultureInfo("fr-FR");
+			Console.WriteLine(Strings.HelloWorld);
 		}
 	}
 }
